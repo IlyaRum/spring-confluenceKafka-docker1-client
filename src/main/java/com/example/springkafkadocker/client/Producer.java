1ee1fd53a13ee3a5a22db49739a0a95a53e6556f
IlyaR <irumayncev@mail.ru>
2021-05-15 23:43:18 +0300

    add Avro for Consumer

--- src/main/java/com/example/springkafkadocker/client/Producer.java
+++ src/main/java/com/example/springkafkadocker/client/Producer.java
@@ -1,30 +1,40 @@
 package com.example.springkafkadocker.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.springkafkadocker.avro.SchemaRepository;
+import lombok.extern.log4j.Log4j2;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
+import java.util.ArrayList;
+import java.util.List;
 
+@Log4j2
+@Component
 @Service
 public class Producer {
 
-    private static final String TOPIC= "my_topic";
+    final private KafkaTemplate template;
 
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    public void writeMessage(String msg){
-
-        this.kafkaTemplate.send(TOPIC, "IN_KEY",  msg);
+    public Producer(KafkaTemplate template) {
+        this.template = template;
     }
 
-    public ListenableFuture<SendResult<String, String>> sendMessage(String topic, String key, String message) {
+    private List<GenericRecord> records = new ArrayList<>();
 
-        return this.kafkaTemplate.send(topic, key, message);
+    public void sendAvroMessage(String topic, String key, String message) {
+        GenericRecord record = new GenericData.Record( SchemaRepository.instance().getSchemaObject());
+        record.put("message", message);
+        record.put( "key", key );
+        record.put( "topic", topic );
+        records.add(record);
+        template.setDefaultTopic( topic );
+        template.sendDefault( key, records );
+        log.info("Producing record: {}", records);
+        template.flush();
+        records.clear();
+
     }
-
-
 }
 
